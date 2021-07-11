cab40ef6a4b784f373600dca960c89fda5359be8
vinukumar-vs <vinu.kumar@tarento.com>
2021-07-11 19:05:50 +0530

    Obersable of interval, time and fromEvent

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,20 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Using "Interval" observable
+    // interval => RxJs observable
+    let intervals = interval(1000);
+    intervals.subscribe(val => console.log("Interval - stream 1 => " + val));
+
+    // Using "Timer" observable
+    // timer => RxJs observable
+    let timers = timer(3000, 1000);
+    timers.subscribe(val => console.log("Timer - stream 1 => " + val));
+
+    // For "Events" on any elements("fromEvent")
+    // fromEvent => RxJs observable
+    var clicks = fromEvent(document, 'click');
+    clicks.subscribe(evt => console.log("document click ", evt));
   }
 
 }
